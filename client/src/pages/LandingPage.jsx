c393037bb2f6ad73585a31fb1d6f0de0a91d73c3
Silvina <101894010+silvina-varela@users.noreply.github.com>
2022-09-16 01:33:31 -0300

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const LandingPage = () => {
+const navigate = useNavigate();
   return (
     <div>
       <div>LandingPage</div>

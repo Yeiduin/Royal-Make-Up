e471afd246dd174875f533d7b3462cf26acb300c
Fátima <insaufaty@gmail.com>
2022-09-15 23:32:51 -0300

    mini commit

--- client/src/components/Details/CardDetails.jsx
+++ client/src/components/Details/CardDetails.jsx
@@ -13,7 +13,9 @@ useEffect( () => {
 
   return (
     <div>
-      <img src={product.image} alt="imagen_producto" />
+      <img src={product.image} alt="imagen_producto" onError={(e) => {
+          e.target.src="https://cdn.shopify.com/s/files/1/0346/1319/8893/collections/elate1.jpg?v=1590520129"
+        }} />
       <h3>{product.name}</h3>
       <p>{product.rating}</p>
       <p>{product.price}</p>

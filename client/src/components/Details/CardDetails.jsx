3a57c906a283089fd7530f29242ba11ac5e1b2ff
Fátima <insaufaty@gmail.com>
2022-09-16 01:25:26 -0300

    agregue botones, colores en detail

--- client/src/components/Details/CardDetails.jsx
+++ client/src/components/Details/CardDetails.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
-import { AddCart } from './AddCart';
-import { useDetailService } from '../../hooks/useDetailService';
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import { AddCart } from "./AddCart";
+import { useDetailService } from "../../hooks/useDetailService";
+import { useEffect } from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const CardDetails = () => {
+  const {
+    getProductById,
+    details: { product },
+  } = useDetailService();
 
-const { getProductById, details: {product} } = useDetailService();
+  useEffect(() => {
+    getProductById();
+  }, []);
 
-useEffect( () => {
-  getProductById();
-},[]);
+  const [values, handleInputChange, reset] = useForm({ colors: "#FFFFFF" });
+  console.log("color" + values.colors);
 
   return (
     <div>
-      <img src={product.image} alt="imagen_producto" onError={(e) => {
-          e.target.src="https://cdn.shopify.com/s/files/1/0346/1319/8893/collections/elate1.jpg?v=1590520129"
-        }} />
+      <img src={product.image} alt="imagen_producto" />
       <h3>{product.name}</h3>
       <p>{product.rating}</p>
       <p>{product.price}</p>
-      {/* pinches colores*/}
-      <AddCart/>
+
+      <select name="colors" id="colors" onChange={handleInputChange}>
+        <option selected="selected">Color</option>
+        {product.colors.length
+          ? product.colors.map((p, index) => {
+              if (index > 5) return "";
+
+              return (
+                <option
+                  key={index}
+                  style={{ backgroundColor: `${p.hex_value}` }}
+                >
+                  {p.colour_name}
+                </option>
+              );
+            })
+          : ""}
+      </select>
+
+      <AddCart />
       <p>{product.description}</p>
     </div>
-  )
-}
+  );
+};

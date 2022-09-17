55895ba146c3919c08a2ff5adbcd617cc4662e7e
Victoria <vickyraineri04@gmail.com>
2022-09-17 18:32:58 -0300

    added comments and users routes

--- api/src/services/usersService.js
+++ api/src/services/usersService.js
@@ -1,15 +1,18 @@
-const { User } = require("../db");
+const { User, Product } = require("../db");
 
 
 /**
- * retorno un usuario si se pasa un id, sino retorno todos
+ * 
+ * @param {*} userId
+ * 
+ * si se pasa un id, retorna ese usuario, sino retorna todos
  */
-async function getAllUsers(id) {
+async function getAllUsers(userId) {
 
     try {
         
-        if(id) {
-            const user = await User.findByPk(id);
+        if(userId) {
+            const user = await User.findByPk(userId);
             return user;
         }
 
@@ -25,7 +28,10 @@ async function getAllUsers(id) {
 
 
 /**
- * añade un usuario
+ * 
+ * @param {*} user objeto con la información del usuario
+ * 
+ * agrega un usuario
  */
 async function addUser(user) {
 
@@ -42,50 +48,59 @@ async function addUser(user) {
 
 
 /**
+ * 
+ * @param {*} userId
+ * 
  * elimina un usuario por id
  */
-async function deleteUser(id) {
+async function deleteUser(userId) {
 
     try {
         
-        await User.destroy({where: {id: id}});
+        await User.destroy({where: {id: userId}});
 
     } catch (error) {
-        
-        throw error;
 
+        throw error;
     }
 
 }
 
 
 /**
- * modifica un usuario por id
+ * 
+ * @param {*} userId
+ * @param {*} newUser
+ * 
+ * modifica propiedades de un usuario por id
  */
-async function modifyUser(id, newUser) {
-    
+async function modifyUser(userId, newUser) {
+
     try {
 
-        const user = await User.findByPk(id);
-        
+        const user = await User.findByPk(userId);
+
         const updatedUser = await user.update(newUser);
-        
+
     } catch (error) {
-        
+
         throw error;
 
     }
-
 }
 
 
 /**
- * modifica el type de un usuario por id 
+ * 
+ * @param {*} userId
+ * @param {*} type
+ * 
+ * cambia el tipo de un usuario por id
  */
-async function changeType(id, type) {
+async function changeType(userId, type) {
 
     try {
-        const user = await User.findByPk(id);
+        const user = await User.findByPk(userId);
 
         await user.update({type: type})
 
@@ -97,10 +112,109 @@ async function changeType(id, type) {
 
 }
 
+
+/**
+ * 
+ * @param {*} userId
+ * @param {*} password
+ * 
+ * cambia la contraseña de un usuario por id
+ */
+async function changePassword(userId, password) {
+
+    try {
+        const user = await User.findByPk(userId);
+
+        await user.update({password: password});
+
+    } catch (error) {
+
+        throw error;
+
+    }
+}
+
+
+/**
+ * 
+ * @param {*} userId
+ * 
+ * retorna todos los productos de la lista de favoritos del usuario por id
+ */
+ async function getUserFavorites(userId) {
+
+    try {
+        const user = await User.findOne({
+            where: {
+                id: userId
+            }
+        });
+
+        return user.favorites;
+
+    } catch (error) {
+
+        throw error;
+
+    }
+}
+
+
+/**
+ * 
+ * @param {*} userId
+ * @param {*} productId
+ * 
+ * agrega productos a la lista de favoritos del usuario por id
+ */
+async function addFavorites(userId, productId) {
+
+    try {
+        const user = await User.findByPk(userId);
+        const product = await Product.findByPk(productId);
+
+        if(!product) throw new Error("Product not found");
+        if(!user.dataValues.favorites.includes(productId)) await user.update({favorites: [...user.dataValues.favorites, productId]});
+        else throw new Error("Product already added to favorites");
+
+    } catch (error) {
+
+        throw error;
+
+    }
+}
+
+
+/**
+ * 
+ * @param {*} userId
+ * @param {*} productId
+ * 
+ * elimina productos de la lista de favoritos del usuario por id
+ */
+async function deleteFavorite(userId, productId) {
+
+    try {
+        const user = await User.findByPk(userId);
+
+        await user.update({favorites: user.dataValues.favorites.filter(fav => fav !== productId)});
+
+    } catch (error) {
+
+        throw error;
+
+    }
+}
+
+
 module.exports = {
     getAllUsers,
     addUser,
     deleteUser,
     modifyUser,
-    changeType
+    changeType,
+    changePassword,
+    addFavorites,
+    deleteFavorite,
+    getUserFavorites
 }

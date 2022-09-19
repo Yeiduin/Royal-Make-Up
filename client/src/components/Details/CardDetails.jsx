d4320ef40e1126bf281b119e31da8230b33820b4
Fátima <insaufaty@gmail.com>
2022-09-18 23:42:44 -0300

    Estilos en el Detalle

--- client/src/components/Details/CardDetails.jsx
+++ client/src/components/Details/CardDetails.jsx
@@ -22,28 +22,29 @@ export const CardDetails = () => {
   return (
     <div className="divDetail" >
       <img src={product.image} alt="imagen_producto" className="imgDetail" />
-      <h3 className="divDetail_h3">{product.name}</h3>
-      <p className="divDetail_p">Rating: {product.rating}</p>
-      <select name="colors" id="colors" onChange={handleInputChange} className="divDetail_select" >
+      <div className="divDetail_div">
+        <h3 className="divDetail_h3">{product.name}</h3>
+        <p className="divDetail_p">Rating: {product.rating}</p>
+        <select name="colors" id="colors" onChange={handleInputChange} className="divDetail_select" >
         <option selected="selected" className="select_option">Color</option>
-          {product.colors.length
-            ? product.colors.map((p, index) => {
-              if (index > 5) return "";
-              return (
-                <option
-                  key={index}
-                  style={{ backgroundColor: `${p.hex_value}` }}
-                >
-                  {p.colour_name}
-                </option>
-              );
-            })
+            {product.colors.length
+              ? product.colors.map((p, index) => {
+                if (index > 5) return "";
+                return (
+                  <option
+                    key={index}
+                    style={{ backgroundColor: `${p.hex_value}` }}
+                  >
+                    {p.colour_name}
+                  </option>
+                );
+              })
           : ""}
-      </select>
+        </select>
       <p className="divDetail_p"><b>$ {product.price}</b></p>
-
       <AddCart />
       <p className="divDetail_description" >{product.description}</p>
+      </div>
     </div>
   );
 };

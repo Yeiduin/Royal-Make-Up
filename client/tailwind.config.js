b3448157982d76f35592b1994f1d350c31779d86
KevinCarrillo93 <kevincarrilloacosta@gmail.com>
2022-09-19 01:15:18 -0500

    styling create form

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,27 +4,30 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    backgroundImage:{
-      'artu': "url('./src/assets/images/ArtuC.png')",
-      'facu': "url('./src/assets/images/FacuC.png')",
-      'fati': "url('./src/assets/images/Fati.jpg')",
-      'kevin': "url('./src/assets/images/Kevin.png')",
-      'lucio': "url('./src/assets/images/LucioC.png')",
-      'silvi': "url('./src/assets/images/Silvi.jpg')",
-      'vicky': "url('./src/assets/images/VickyC.png')",
-      'yei': "url('./src/assets/images/YeiC.png')",
+    extend:{
+      backgroundImage:{
+        'artu': "url('./src/assets/images/ArtuC.png')",
+        'facu': "url('./src/assets/images/FacuC.png')",
+        'fati': "url('./src/assets/images/Fati.jpg')",
+        'kevin': "url('./src/assets/images/Kevin.png')",
+        'lucio': "url('./src/assets/images/LucioC.png')",
+        'silvi': "url('./src/assets/images/Silvi.jpg')",
+        'vicky': "url('./src/assets/images/VickyC.png')",
+        'yei': "url('./src/assets/images/YeiC.png')",
+      },
+      colors:{
+        primary: '#556353',
+        secondary: '#FBA744',
+        tertiary: '#F6F5EC',
+      },
+      fontFamily:{
+        sans: ['Poppins'],
+        serif:['Marcellus SC'],
+      }
     },
-    colors:{
-      primary: '#556353',
-      secondary: '#FBA744',
-      tertiary: '#F6F5EC',
-    },
-    fontFamily:{
-      sans: ['Poppins'],
-      serif:['Marcellus SC'],
-    }
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
   ],
 }

594d2e83131aa49fdcd6a02b47b8c2a3015a4ee8
Fátima <insaufaty@gmail.com>
2022-09-18 21:52:02 -0300

    Estilos en el Detalle

--- client/src/components/Details/AddCart.jsx
+++ client/src/components/Details/AddCart.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './estilosDetail/AddCart.css';
 
 export const AddCart = () => {
 
   return (
-    <div>
-      <button>-</button>
-      <p>1</p>
-      <button>+</button>
-      <button>ADD TO CART</button>
+    <div className='divAddCart'>
+      <div className='divAddCart_div'>
+        <button className='div_button1'>-</button>
+        <p className='dic_p'>1</p>
+        <button className='div_button2'>+</button>
+      </div>
+      
+      <button className='divAddCart_button'>ADD TO CART</button>
     </div>
   )
 }

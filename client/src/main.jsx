e0c02a2540888f69cd6cef0c63273ca5f390eb99
KevinCarrillo93 <98859653+KevinCarrillo93@users.noreply.github.com>
2022-09-23 17:08:23 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,12 @@ import { Provider } from "react-redux";
 import './index.css';
 import App from "./App";
 import { store } from "./redux/store";
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>

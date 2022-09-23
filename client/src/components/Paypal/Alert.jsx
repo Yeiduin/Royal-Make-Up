821a6ac97c5b93215ece2ae3c6721b8fcb5336c2
finstory <facu995electro@hotmail.com>
2022-09-23 19:07:37 -0300

    agregado de checkuot

--- client/src/components/Paypal/Alert.jsx
+++ client/src/components/Paypal/Alert.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { useNav } from "../../../hooks/useNav";
+import { useNav } from "../../hooks/useNav";
+
 import "./alertPaypal.css";
 
 export const Alert = ({ msg, ok }) => {

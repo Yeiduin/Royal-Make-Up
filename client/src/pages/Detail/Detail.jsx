ee2ad0ebee502fa2542c6cc8cc247551975a3111
silvina-varela <varelasilvina@gmail.com>
2022-09-28 15:07:23 -0300

    fixed filters,  changed input for detail colors

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -3,32 +3,48 @@ import { DetailCard } from "../../components/DetailCard/DetailCard";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {
+  getHomeProducts,
   getProductById,
   getProducts,
   resetDetail,
 } from "../../redux/actions/index";
 import { SwiperComponent } from "../../components/SwiperComponent/SwiperComponent";
+import { Loader } from "../../components/Loader/Loader";
+
 
 export const Detail = () => {
-  const { productDetail, productType } = useSelector((state) => state);
+  const { productDetail, productType, listPopular } = useSelector(
+    (state) => state
+  );
   const { id } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProductById(id));
     dispatch(getProducts());
+    dispatch(getHomeProducts());
     return () => dispatch(resetDetail());
   }, [dispatch, id]);
 
-  
-  return (
-    <div>
-      {productDetail && <DetailCard {... productDetail} />}
-      {productType.length && 
-      <div className="mx-auto max-w-2xl lg:max-w-screen-2xl">
-        <h2 className="text-xl pb-6">You might also like...</h2>
-        <SwiperComponent array={productType} />
+  if(!productDetail.name?.length){
+    return(
+      <div className="flex flex-row justify-center space-x-20 pt-40">
+      <div className="mb-12">
+      <Loader />
       </div>
-      }
+      </div>
+    )
+  } else return (
+    <div>
+      {productDetail && <DetailCard {...productDetail} />}
+        <div className="mx-auto max-w-2xl lg:max-w-screen-2xl">
+        <h2 className="text-xl pb-6">You might also like...</h2>
+
+        {productType?.length ? (
+          <SwiperComponent array={productType} />
+        ) : (
+          <SwiperComponent array={listPopular} />
+        )}
+      </div>
     </div>
   );
 };

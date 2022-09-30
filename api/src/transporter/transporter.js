603ab67ba4537ab3bfde56a8324df9bb502225d3
Victoria <vickyraineri04@gmail.com>
2022-09-30 03:47:47 -0300

    added dotenv config

--- api/src/transporter/transporter.js
+++ api/src/transporter/transporter.js
@@ -1,13 +1,14 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 const { DB_EMAIL, DB_PASS } = process.env; 
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true,
     auth: {
-      user: DB_EMAIL, // generated ethereal user
-      pass: DB_PASS, // generated ethereal password
+      user: DB_EMAIL,
+      pass: DB_PASS,
     },
   });
   

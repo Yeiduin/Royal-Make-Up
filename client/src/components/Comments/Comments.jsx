15fb33ab83a64a4e6a296cf7d9c05ae2b3722347
arturokaadu <arturokaadu2@gmail.com>
2022-10-02 15:53:21 -0300

    comments without styling

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -28,7 +28,6 @@ export const Comments = (product) => {
   
   const [comment, setComment] = useState("");
   const [ondelete, onsetDelete] = useState("");
-  const [stars, setStars] = useState(0);
 
   const handleOnChange = (e) => {
     setComment(e.target.value);
@@ -82,12 +81,7 @@ export const Comments = (product) => {
   };
 
 
-  const handleStars = (e) => {
-   
-    setStars(e.target.value);
 
-  };
-  
    
 /*    useEffect((id) => {
     dispatch(getUserId(id));
@@ -100,16 +94,15 @@ export const Comments = (product) => {
     <div id='comments'>
       {allCommentsByProduct &&
         allCommentsByProduct.map((e) => {
-          dispatch(getUserId(e.UserId))
-          console.log(userId);
+        
           return (
             <div key={e.id} className='flex justify-around bg-tertiary rounded-2xl p-4 mx-60'>
               <div className="flex flex-col">
                 <div className="flex">
                   <i className="material-icons">person_pin</i>
                   
-                   <span>{userId.username && userId.username}</span> 
-                   <img src={userId.img && userId.img} alt="userImg"  /> 
+                   <span>{e.User.username}</span> 
+                   <img src={e.User.img} alt="userImg"  /> 
                 </div>     
                {/* <span>⭐⭐⭐⭐⭐</span> */}
               </div>
@@ -130,46 +123,6 @@ export const Comments = (product) => {
         <div className="flex flex-col items-center gap-4 py-8 ">
           <h1>Post Your Comments</h1>
           
-
-
-          <div onChange={(e) => handleStars(e)}>
-            
-      <fieldset class="rating">
-        <input type="radio" id="star5" name="rating" value="5" />
-        <label class="full" for="star5" title="Awesome - 5 stars"></label>
-        <input type="radio" id="star4half" name="rating" value="4.5" />
-        <label
-          class="half"
-          for="star4half"
-          title="Pretty good - 4.5 stars"
-        ></label>
-        <input type="radio" id="star4" name="rating" value="4" />
-        <label class="full" for="star4" title="Pretty good - 4 stars"></label>
-        <input type="radio" id="star3half" name="rating" value="3.5" />
-        <label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-        <input type="radio" id="star3" name="rating" value="3" />
-        <label class="full" for="star3" title="Meh - 3 stars"></label>
-        <input type="radio" id="star2half" name="rating" value="2.5" />
-        <label
-          class="half"
-          for="star2half"
-          title="Kinda bad - 2.5 stars"
-        ></label>
-        <input type="radio" id="star2" name="rating" value="2" />
-        <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
-        <input type="radio" id="star1half" name="rating" value="1.5" />
-        <label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-        <input type="radio" id="star1" name="rating" value="1" />
-        <label class="full" for="star1" title="Sucks big time - 1 star"></label>
-        <input type="radio" id="starhalf" name="rating" value="0.5" />
-        <label
-          class="half"
-          for="starhalf"
-          title="Sucks big time - 0.5 stars"
-        ></label>
-      </fieldset>
-    </div>
-
 
 
           
